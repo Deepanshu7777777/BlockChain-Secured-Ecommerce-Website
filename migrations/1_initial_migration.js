fe936c39b12b19e34c66bb4273f6b9f303e5cb33
Deepanshu <96643131+Deepanshu7777777@users.noreply.github.com>
2023-03-26 16:13:28 +0530

    Basic Usage Defines
    Basic Usage Defines

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,3 +1,6 @@
+//This file is used to deploy the Migrations smart contract to the blockchain.
+//the Migrations smart contract is imported using the artifacts.require function, which looks for the Migrations contract in the ./Migrations.sol file.
+
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer) {
