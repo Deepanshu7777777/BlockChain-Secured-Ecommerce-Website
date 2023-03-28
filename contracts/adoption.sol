8711c7a040374108bb8de64614dccdd8394e8e09
Deepanshu <96643131+Deepanshu7777777@users.noreply.github.com>
2023-03-28 20:43:08 +0530

    Solidity Functions Modified & Added.
    Solidity Functions Modified & Added.

--- contracts/adoption.sol
+++ contracts/adoption.sol
@@ -1,23 +1,22 @@
 //pragma solidity ^0.8.19;
 pragma solidity ^0.5.0;  //  minimum version of solidity required at the top of contract
-//pragma means the additional info. the  compiler only  care about .
+
 
 // creating 1st contract 
-contract Adoption{                  //it is a stactical type language, meaning data types like string integers are added must be defined to address unique value ,stored as 20 byte value  ,used for sending and receiving ethers
+contract Adoption{                 
     address[16] public adopters;    // adopter is an array contain ETH ADDRESS.    
                                      
 
 //Creating function to fill the address in adopters array 
     function adopt(uint itemId) public returns(uint){ // taking item id and return it also 
-        require(itemId >= 0 && itemId <=15);           // checking if item id is in range    of our adopters array // require is condion 
-
+        require(itemId >= 0 && itemId <=15);           
         adopters[itemId]= msg.sender; 
         return itemId;               
     }
 
     //writing 2nd function 
     // function to retrive the adopters ,,  array getters only a single valye from the given key so,
-    // we write this fucntion to return the entire array  
+    
 
     function getadopters() public view returns (address[16] memory){  
         return adopters;                                             
@@ -75,17 +74,16 @@ function additem() public onlyOwner {
 ////////////A function to allow item owners to update their products name:///////
 
 // This function adds a new products/item items struct to the contract, which includes fields for the items  Type,Stock , and location. 
-// The updateitemName function allows the owner of a item to update the items Type (but not its Stock or Location). 
-//The require statement ensures that only the owner of the item can update its name.
+.
    struct item {            
     string Type;
     uint8 Stock;
     string Location;
 }
 
-item[] public items;      //This declares a dynamic array called items that stores instances of the item struct, accessable form outside the contract.
+item[] public items;     
 
-function updateitemName(uint itemId, string memory newType) public {          //two arguments: itemId and newType. The function updates the Type property of the item struct at the specified itemId index in the items array with the new value of newType.   //The memory keyword is used to specify that newType should be stored in memory rather than storage. This is because newType is a function argument and is only needed for the duration of the function call, whereas storage variables persist beyond the lifetime of the function.
+function updateitemName(uint itemId, string memory newType) public {         
     require(msg.sender == adopters[itemId], "Only the item owner can update the items name");
     items[itemId].Type = newType;
 }
